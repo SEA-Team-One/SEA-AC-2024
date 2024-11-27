d522446b8243825bd962105b08d29477ac35c7ba
sea5 <sea5@ukk2tq2d02ty.bskyb.com>
2024-11-27 14:04:31 +0000

    State management

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,17 @@
 import {Box, Button, Slider, Stack, Typography} from '@mui/material'
+import TopButton from './components/Button/TopButton';
+import axios from 'axios';
+import React, { createContext, useState } from 'react'
+
 
 function App() {
+   const [calmScore, setCalmScore] = useState(0);
+   const [cheerfulScore, setCheerfulScore] = useState(0);
+   const [awakeScore, setAwakeScore] = useState(0);
+   const [fearlessScore, setFearlessScore] = useState(0);
+   const [movieList, setMovieList] = useState([]);
+   const URL = "http://localhost:3000"
+   
 
   return (
     <Stack direction={'column'} spacing={4}>
@@ -18,27 +29,49 @@ function App() {
         <Typography>Mood Slider</Typography>
       </Stack>
       <Stack direction={'row'} spacing={4}>
-        <Button variant='contained'>Hello</Button>
-        <Button variant='contained'>Hello</Button>
-        <Button variant='contained'>Hello</Button>
-        <Button variant='contained'>Hello</Button>
+        <TopButton text="Use Webcam" styling="contained" onPressed={() => {
+            
+        }}/>
+        <TopButton text="Choose File" styling="outlined" onPressed={() => {}}/>   
+        <TopButton text="Submit" styling="contained" onPressed={() => {
+            axios.post({URL}+'/movies', {
+                calm_score: calmScore,
+                cheerful_score: cheerfulScore,
+                awake_score: awakeScore,
+                fearless_score: fearlessScore
+            }).then(function (response) {
+                console.log(response);
+                setMovieList(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }}/>
       </Stack>
       <Stack direction={'column'} spacing={4}>
         <Stack direction={'row'} spacing={4}>
           <Typography>Calm</Typography>
-          <Slider></Slider>
+          <Slider value={calmScore} min={1} max={10} step={1} onChange={(e, val) => {
+            setCalmScore(val);
+          }}></Slider>
         </Stack>
         <Stack direction={'row'}>
           <Typography>Cheerful</Typography>
-          <Slider></Slider>
+          <Slider value={cheerfulScore} min={1} max={10} step={1} onChange={(e, val) => {
+            setCheerfulScore(val);
+          }}></Slider>
         </Stack>
         <Stack direction={'row'}>
           <Typography>Awake</Typography>
-          <Slider></Slider>
+          <Slider value={awakeScore} min={1} max={10} step={1} onChange={(e, val) => {
+            setAwakeScore(val);
+          }}></Slider>
         </Stack>
         <Stack direction={'row'}>
           <Typography>Fearless</Typography>
-          <Slider></Slider>
+          <Slider value={fearlessScore}  min={1} max={10} step={1} onChange={(e, val) => {
+            setFearlessScore(val);
+          }}></Slider>
         </Stack>
       </Stack>
       <Stack direction={'row'} spacing={4}>
